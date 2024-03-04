3c895a7bbe3b5e36977bee3a7032b4d7acea1c51
briz1 <my1queen8my1king@gmail.com>
2024-03-05 03:00:00 +0700

    Not work

--- src/main/java/pro/sky/skyprospringcalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorServiceImpl.java
@@ -3,10 +3,29 @@ package pro.sky.skyprospringcalculator;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorServiceImpl {
+public class CalculatorServiceImpl implements CalculatorService{
 
     public String hello() {
         return "Добро пожаловать в калькулятор!";
     }
+
+    @Override
+    public int summa(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public int subtraction(int num1, int num2){
+        return num1-num2;
+    }
+
+    public int multiplication(int num1, int num2) {
+        return num1*num2;
+    }
+
+    public int division(int num1, int num2) {
+        return num1/num2;
+    }
+
+
 
 }
